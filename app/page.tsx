dcdcae20a6fe3b23e0941c1c3ea88c4a386ba31a
kathyhua1 <165853854+kathyhua1@users.noreply.github.com>
2025-10-07 21:38:11 -0700

    Update subtitle to 'Airbnb Case Study'

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             Kathy Hua
           </h1>
           <p className="text-lg text-gray-600 max-w-2xl mx-auto">
-            Case Study: Airbnb
+            Airbnb Case Study
           </p>
         </div>
 
